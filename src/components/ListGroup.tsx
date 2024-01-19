f5e4a27c7a11c74bbf861491accc560232d182fa
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-01-19 21:33:19 +0100

    ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,12 @@
 function ListGroup() {
-  const items = [
-    "New York",
-    "Los Angeles",
-    "Chicago",
-    "Houston",
-    "Philadelphia",
-  ];
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"];
+  items = [];
+  const message = items.length === 0 && <p>There are no items in the list</p>;
+
   return (
     <>
       <h1>List</h1>
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
