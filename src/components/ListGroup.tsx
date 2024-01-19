f4bcc08c9b8e570f0936ec5a4129d55cfeb79661
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-01-19 21:16:50 +0100

    ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,20 @@
-function ListGroup(){
-    return (
-            <h1>List Group</h1>
-    )
+function ListGroup() {
+  const items = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Philadelphia",
+  ];
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 export default ListGroup;
