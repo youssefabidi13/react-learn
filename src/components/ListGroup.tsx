e0c4bac0bc8d61d2edd7b3e8a446640b7c624f58
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-01-19 21:17:32 +0100

    ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -11,7 +11,7 @@ function ListGroup() {
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li className="list-group-item">{item}</li>
         ))}
       </ul>
     </>
