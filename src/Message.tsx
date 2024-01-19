f646e16cc3e41964d6f31faff008198987401992
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-01-19 21:04:26 +0100

    ListGroup component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,14 @@
 function Message(){
+    const name = 'ABIDI Youssef';
+    if(name.length > 0){
     return (
-            <h1>Hello world</h1>
+            <h1>Hello {name}</h1>
     )
+    } else {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
 }
 
 export default Message;
