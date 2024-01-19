8d5b936cf40356a3c71226a949ccca83e1b7fab6
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-01-19 21:57:37 +0100

    ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,26 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   const message = items.length === 0 && <p>There are no items in the list</p>;
-  const handelClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {message}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li key={item} className="list-group-item" onClick={handelClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
