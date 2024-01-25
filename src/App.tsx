5abcf304f1bc68672fb3b5e404da87b2b847c7b1
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-01-25 17:15:39 +0100

    using immer to update state

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,45 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 import { BiBulb } from "react-icons/bi";
-
+import produce from "immer";
+import { set } from "immer/dist/internal";
 function App() {
-  const handleClick = () => {
-    setAlertVisibility(true);
-  };
+  // const handleClick = () => {
+  //   setAlertVisibility(true);
+  // };
 
-  const [alertVisible, setAlertVisibility] = useState(false);
-  const [items, setItems] = useState(["Miami", "New York", "Boston"]);
-  function handleSelectItem(item: string) {
-    console.log(item);
-  }
+  // const [alertVisible, setAlertVisibility] = useState(false);
+  // const [items, setItems] = useState(["Miami", "New York", "Boston"]);
+  // function handleSelectItem(item: string) {
+  //   console.log(item);
+  // }
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // const [person, setPerson] = useState({
+  //   firstname: "",
+  //   lastname: "",
+  // });
+  // const [isLoading, setIsLoading] = useState(false);
+  const [bugs, setBugs] = useState([
+    {
+      id: 1,
+      title: "Bug 1",
+      fixed: false,
+    },
+    {
+      id: 2,
+      title: "Bug 2",
+      fixed: false,
+    },
+  ]);
+  const handleClick = () => {
+    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce((draft) => {
+        draft[0].fixed = true;
+      })
+    );
+  };
   return (
     <div>
       {/* <Alert>
@@ -28,7 +56,7 @@ function App() {
         items={items}
         onSelectItem={handleSelectItem}
       /> */}
-      <BiBulb color="red" />
+      {/* <BiBulb color="red" /> */}
     </div>
   );
 }
