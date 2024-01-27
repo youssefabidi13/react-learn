0329e59f36401f39b3c575d67a88d4985fcc388a
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-01-27 16:32:34 +0100

    form submiting use state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,21 @@
-import React, { HtmlHTMLAttributes, useRef } from "react";
+import React, { HtmlHTMLAttributes, useRef, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  // const nameRef = useRef<HTMLInputElement>(null);
+  // const ageRef = useRef<HTMLInputElement>(null);
+  // const person = { name: "", age: 0 };
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (nameRef.current && ageRef.current) {
-      person.name = nameRef.current.value;
-      person.age = ageRef.current.valueAsNumber;
-      console.log(person);
-    }
+    // if (nameRef.current && ageRef.current) {
+    //   person.name = nameRef.current.value;
+    //   person.age = ageRef.current.valueAsNumber;
+    //   console.log(person);
+    // }
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -18,13 +23,32 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        {/* <input ref={nameRef} id="name" type="text" className="form-control" /> */}
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        {/* <input ref={ageRef} id="age" type="number" className="form-control" /> */}
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: event.target.value })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+
         <button className="btn btn-primary" type="submit">
           Submit
         </button>
