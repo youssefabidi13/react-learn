664ba91ad04b2d9df296e6db1ea80592dbaffec5
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-01-27 17:17:36 +0100

    form submiting using hook form and zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,21 @@
 import React, { HtmlHTMLAttributes, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-interface FormData {
-  name: string;
-  age: number;
-}
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+const schema = z.object({
+  name: z.string().min(3).max(10),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .positive()
+    .int()
+    .min(18)
+    .max(100),
+});
+// interface FormData {
+//   name: string;
+//   age: number;
+// }
+type FormData = z.infer<typeof schema>;
 const Form = () => {
   // const nameRef = useRef<HTMLInputElement>(null);
   // const ageRef = useRef<HTMLInputElement>(null);
@@ -26,7 +38,7 @@ const Form = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
@@ -39,17 +51,19 @@ const Form = () => {
           //   setPerson({ ...person, name: event.target.value })
           // }
           // value={person.name}
-          {...register("name", { required: true, minLength: 3 })}
+          //{...register("name", { required: true, minLength: 3 })} without zod
+          {...register("name")} // with zod
           id="name"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
+        {/* {errors.name?.type === "required" && (
           <p className="text-danger">The name field is required</p>
-        )}
-        {errors.name?.type === "minLength" && (
+        )} */}
+        {/* {errors.name?.type === "minLength" && (
           <p className="text-danger">The name must be at least 3 characters</p>
-        )}
+        )}  without zod*/}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
       <div className="mb3">
         <label htmlFor="age" className="form-label">
@@ -61,11 +75,12 @@ const Form = () => {
           //   setPerson({ ...person, age: event.target.value })
           // }
           // value={person.age}
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
 
         <button className="btn btn-primary" type="submit">
           Submit
